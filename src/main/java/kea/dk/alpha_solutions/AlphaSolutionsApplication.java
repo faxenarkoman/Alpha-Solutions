fcf8d3ca82b6970cd22c5b5f718cfeb65fb713f3
ArthurOS <79909701+faxenarkoman@users.noreply.github.com>
2023-05-01 11:37:41 +0200

    initial commit

---
+++ src/main/java/kea/dk/alpha_solutions/AlphaSolutionsApplication.java
@@ -0,0 +1,15 @@
+package kea.dk.alpha_solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaSolutionsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AlphaSolutionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/dk/alpha_solutions/AlphaSolutionsApplicationTests.java
@@ -0,0 +1,15 @@
+package kea.dk.alpha_solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaSolutionsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
