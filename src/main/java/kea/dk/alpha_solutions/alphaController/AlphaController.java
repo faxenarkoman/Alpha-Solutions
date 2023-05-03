0264713a8e8e54425c270fdb6021c130b4bc2ff8
ArthurOS <79909701+faxenarkoman@users.noreply.github.com>
2023-05-03 10:05:12 +0200

    Login page done. Added skeleton for database connection

--- src/main/java/kea/dk/alpha_solutions/alphaController/AlphaController.java
+++ src/main/java/kea/dk/alpha_solutions/alphaController/AlphaController.java
@@ -1,5 +1,6 @@
 package kea.dk.alpha_solutions.alphaController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import kea.dk.alpha_solutions.model.User;
 import org.springframework.stereotype.Controller;
@@ -14,25 +15,25 @@ public class AlphaController
     @GetMapping(value ="/")
     public String login()
     {
-        return "login.html";
+        return "login";
     }
 
 
-    @PostMapping(value = "/check-login")
-    public String checkLogin(@RequestParam("username") String mail, @RequestParam("password") String password, HttpServletRequest request)
-    {
-        User tmpUser = new User(mail,password);
-        HttpSession session;
-        if(tmp.checkUser(mail,password))
-        {
-            session = request.getSession();
-            session.setAttribute("username", tmpUser.getMail());
-            session.setAttribute("password", tmpUser.getPassword());
-            return "redirect:/index";
-        }
-        else
-        {
-            return "redirect:/";
-        }
-    }
+    //@PostMapping(value = "/check-login")
+    //public String checkLogin(@RequestParam("username") String mail, @RequestParam("password") String password, HttpServletRequest request)
+    //{
+    //  User tmpUser = new User(mail,password);
+    //  HttpSession session;
+    //  if(tmp.checkUser(mail,password))
+    //  {
+    //      session = request.getSession();
+    //      session.setAttribute("username", tmpUser.getMail());
+    //      session.setAttribute("password", tmpUser.getPassword());
+    //      return "redirect:/index";
+    //  }
+    //  else
+    //  {
+    //      return "redirect:/";
+    //  }
+    //}
 }
--- src/main/java/kea/dk/alpha_solutions/alphaRepository/alphaRepository.java
+++ src/main/java/kea/dk/alpha_solutions/alphaRepository/alphaRepository.java
@@ -1,5 +1,14 @@
 package kea.dk.alpha_solutions.alphaRepository;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+@Repository
 public class alphaRepository
 {
+        @Value("${spring.datasource.url}")
+        private String DB_URL;
+        @Value("${spring.datasource.username}")
+        private String UID;
+        @Value("${spring.datasource.password}")
+        private String PWD;
 }
