cf95c477d976f8ffe0fca424a8d06d63dca413f0
ArthurOS <79909701+faxenarkoman@users.noreply.github.com>
2023-05-10 11:28:50 +0200

    Login works (yay)

--- src/main/java/kea/dk/alpha_solutions/alphaController/AlphaController.java
+++ src/main/java/kea/dk/alpha_solutions/alphaController/AlphaController.java
@@ -1,12 +1,14 @@
 package kea.dk.alpha_solutions.alphaController;
 
 import jakarta.servlet.http.HttpSession;
+import kea.dk.alpha_solutions.alphaRepository.AlphaRepositoryUser;
+import kea.dk.alpha_solutions.model.User;
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 
 @Controller
 public class AlphaController
@@ -17,18 +19,27 @@ public class AlphaController
         return "login";
     }
 
-
+    @Autowired
+    private AlphaRepositoryUser alphaRepositoryUser;
     @PostMapping("/login")
     public String doLogin(@RequestParam("email") String email, HttpSession session,
-                          @RequestParam("password") String password){
-        // Kryptering af password vha. hashing via BCrypt
+                          @RequestParam("password") String password) {
         String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        System.out.println("Email: " + email);
+        System.out.println("Plain password: " + password);
+        System.out.println("Hashed password: " + hashedPassword);
 
-        //set attributer i session
-        session.setAttribute("email", email);
-        // Store hashed password in session rather than password
-        session.setAttribute("password", hashedPassword);
-        return "redirect:/index";
+        User user = alphaRepositoryUser.getUserByEmail(email);
+
+        if (user != null && BCrypt.checkpw(password, user.getPassword())) {
+            // If user exists and password matches, set attributes in session
+            session.setAttribute("email", email);
+            session.setAttribute("password", user.getPassword()); // Store hashed password in session
+            return "redirect:/index";
+        } else {
+            // If user does not exist or password does not match, return to login page with error message
+            return "login?error";
+        }
     }
 
     @GetMapping("/index")
