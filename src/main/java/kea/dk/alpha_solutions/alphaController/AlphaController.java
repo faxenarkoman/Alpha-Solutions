d5bc6d0218e7885104c36034389a64d138c59e60
ArthurOS <79909701+faxenarkoman@users.noreply.github.com>
2023-05-11 10:40:12 +0200

    Overwritten changes

--- src/main/java/kea/dk/alpha_solutions/alphaController/AlphaController.java
+++ src/main/java/kea/dk/alpha_solutions/alphaController/AlphaController.java
@@ -1,11 +1,14 @@
 package kea.dk.alpha_solutions.alphaController;
 
 import jakarta.servlet.http.HttpSession;
+import kea.dk.alpha_solutions.alphaRepository.AlphaRepositoryProject;
 import kea.dk.alpha_solutions.alphaRepository.AlphaRepositoryUser;
+import kea.dk.alpha_solutions.model.Project;
 import kea.dk.alpha_solutions.model.User;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AlphaController
 {
+    AlphaRepositoryProject alphaRepositoryProject = new AlphaRepositoryProject();
+
+    public AlphaController(AlphaRepositoryProject alphaRepositoryProject)
+    {
+        this.alphaRepositoryProject = alphaRepositoryProject;
+    }
+
     @GetMapping(value ="/")
     public String login()
     {
@@ -46,4 +56,47 @@ public class AlphaController
     public String index(){
         return "index";
     }
+    @GetMapping("project")
+    public String createProjectList(Model model)
+    {
+        model.addAttribute("alphas", alphaRepositoryProject.getAll());
+        return "index";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(
+            @RequestParam("project-UserID") int newUserID,
+            @RequestParam("project-ProjectID") int newProjectID,
+            @RequestParam("project-ProjectTitle") String newProjectTitle,
+            @RequestParam("project-ProjectDescription") String newDescripton,
+            @RequestParam("project-Deadline") int newNrOfHours,
+            @RequestParam("project-NrOfUsers") int newNrOfUsers,
+            @RequestParam("project-ProjectPrice") int newProjectPrice,
+            @RequestParam("project-HoursPrDay") int newHoursPrDay){
+
+        Project newProject = new Project();
+
+        newProject.setUserID(newUserID);
+        newProject.setProjectID(newProjectID);
+        newProject.setProjectTitle(newProjectTitle);
+        newProject.setProjectDescription(newDescripton);
+        newProject.setNrOfHours(newNrOfHours);
+        newProject.setNrOfHours(newNrOfUsers);
+        newProject.setProjectPrice(newProjectPrice);
+        newProject.setHoursPerDay(newHoursPrDay);
+
+        //gem nyt produkt
+        AlphaRepositoryProject alphaRepositoryProject = new AlphaRepositoryProject();
+        alphaRepositoryProject.addProject(newProject);
+
+        //tilbage til produktlisten
+        return "redirect:/index";
+    }
+
+    @GetMapping("/create")
+    public String viewProductList() {
+        return "/create";
+    }
+
+
 }
