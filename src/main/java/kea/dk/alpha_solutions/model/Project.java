bca6394044d98807f25229e2010685ef5b185270
ArthurOS <79909701+faxenarkoman@users.noreply.github.com>
2023-05-26 11:26:11 +0200

    update createProject

--- src/main/java/kea/dk/alpha_solutions/model/Project.java
+++ src/main/java/kea/dk/alpha_solutions/model/Project.java
@@ -8,9 +8,7 @@ public class Project {
     private String projectTitle;
     private String projectDescription;
     private String deadline;
-    private int nrOfUsers;
-    private int nrOfHours;
-    private double projectPrice;
+
     private int hoursPerDay;
 
     private Set<Task> tasks = new HashSet<>();
@@ -38,15 +36,11 @@ public class Project {
     public Project() {
     }
 
-    public Project(int projectID, String projectTitle, String projectDescription, String deadline, int nrOfUsers,
-                   int nrOfHours, double projectPrice, int hoursPerDay) {
+    public Project(int projectID, String projectTitle, String projectDescription, String deadline, int hoursPerDay) {
         this.projectID = projectID;
         this.projectTitle = projectTitle;
         this.projectDescription = projectDescription;
         this.deadline = deadline;
-        this.nrOfUsers = nrOfUsers;
-        this.nrOfHours = nrOfHours;
-        this.projectPrice = projectPrice;
         this.hoursPerDay = hoursPerDay;
     }
 
@@ -82,30 +76,6 @@ public class Project {
         this.deadline = deadline;
     }
 
-    public int getNrOfUsers() {
-        return nrOfUsers;
-    }
-
-    public void setNrOfUsers(int nrOfUsers) {
-        this.nrOfUsers = nrOfUsers;
-    }
-
-    public int getNrOfHours() {
-        return nrOfHours;
-    }
-
-    public void setNrOfHours(int nrOfHours) {
-        this.nrOfHours = nrOfHours;
-    }
-
-    public double getProjectPrice() {
-        return projectPrice;
-    }
-
-    public void setProjectPrice(double projectPrice) {
-        this.projectPrice = projectPrice;
-    }
-
     public int getHoursPerDay() {
         return hoursPerDay;
     }
@@ -121,9 +91,6 @@ public class Project {
                 ", projectTitle='" + projectTitle + '\'' +
                 ", projectDescription='" + projectDescription + '\'' +
                 ", deadline='" + deadline + '\'' +
-                ", nrOfUsers=" + nrOfUsers +
-                ", nrOfHours=" + nrOfHours +
-                ", projectPrice=" + projectPrice +
                 ", hoursPerDay=" + hoursPerDay +
                 '}';
     }
